7a83d6cd197aba72ea65eb756dea0bb6affb5beb
Thbop <75861321+thbop@users.noreply.github.com>
2025-05-25 18:44:31 -0400

    Added license information

--- include/vector_t.h
+++ include/vector_t.h
@@ -20,38 +20,3 @@
 * SOFTWARE.
 */
 
-#ifndef MODEL_H
-#define MODEL_H
-
-
-#include "tfile.h"
-#include "tstring.h"
-
-// A CPU representation of the model to be loaded onto the GPU later
-typedef struct {
-    char *name;
-    float *verticies;
-    unsigned int vertexCount;
-    unsigned int *indicies;
-    unsigned int indexCount;
-} Model;
-
-// Loads the only object in a Wavefront .obj file (ensure that there is only
-// one object). Also make sure that the faces are triangles.
-Model *LoadModel( const char *modelFileName ) {
-    char *data = TFileOpen( modelFileName );
-    if ( data == NULL )
-        return NULL;
-
-    int lineCount;
-    char **lines = TStringSplit( data, '\n', &lineCount );
-
-    for ( int i = 0; i < lineCount; i++ ) {
-
-    }
-
-    TStringFreeStringArray( lines, lineCount );
-    free( data );
-}
-
-#endif
